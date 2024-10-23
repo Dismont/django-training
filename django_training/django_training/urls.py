d8d1904418ca178388c7d38c933d2ced11c40f7e
Dismont <shin195802@gmail.com>
2024-10-24 02:13:31 +0300

    Добавили конверторы int, slug, arch_year[0-9]{4} для спец. ссылок и запросов на сайт

--- django_training/django_training/urls.py
+++ django_training/django_training/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from topic import views
 
 
@@ -8,6 +8,10 @@ urlpatterns = [
 
     # URLS FOR APPLICATION TOPIC
 
-    path("topic/", views.m_topic),          # http://127.0.0.1:8080/topic/ - основная страница
-    path("topic/disc/",views.topic_disc)    # http://127.0.0.1:8080/topic/disc/ - страница описания проекта
+    path("topic/", views.m_topic),                                             # http://127.0.0.1:8080/topic/ - основная страница
+    path("topic/disc/", views.topic_disc),                                     # http://127.0.0.1:8080/topic/disc/ - страница описания проекта
+    path("topic/disc/<int:int_disc>/", views.topic_disc_by_int),               # http://127.0.0.1:8080/topic/disc/1..X/ - Балуемся с пользовательским конвертором (integer 0-XXXXX)
+    path("topic/disc/<slug:slug_disc>/", views.topic_disc_by_slug),            # http://127.0.0.1:8080/topic/disc/бла-бла-бла/ - Балуемся с пользовательским конвертором (slug {text} without "/")
+    re_path(r"^topic/archive/(?P<arch_year>[0-9]{4})/",views.topic_archi_year),     # http://127.0.0.1:8080/topic/archive/1941/ - Балуемся с пользовательским конвертором
+    # (Кастомная переменная arch_year, символы 0..9, 4 числа, примеры:{1234,9876,1941})
 ]
--- django_training/topic/views.py
+++ django_training/topic/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.http import request,HttpResponse
+from django.http import request, HttpResponse
+
 
 def m_topic(request):
-    return render(request,"m_topic.html")
+    return render(request, "m_topic.html")
+
 
 def topic_disc(request):
     return render(request, "topic_disc.html")
 
+
+def topic_disc_by_int(request, int_disc):
+    return HttpResponse(f"ID DISCRIPTION: {int_disc}")
+    # return render(request, "topic_disc_conv.html", context=id_disc) ---> Позже доделаю...
+
+def topic_disc_by_slug(request, slug_disc):
+    return HttpResponse(f"CAUGHT DISCRIPTION: {slug_disc}")
+
+def topic_archi_year(request, arch_year):
+    return HttpResponse(f"CAUGHT YEAR: {arch_year}")
 
