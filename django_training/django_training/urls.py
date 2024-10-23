3b119e5f86a3b5dd4457f4d6d3873e7fb685c8c1
Dismont <shin195802@gmail.com>
2024-10-24 01:26:16 +0300

    Добавил пару комментариев в ~/urls.py основного проекта

--- django_training/django_training/urls.py
+++ django_training/django_training/urls.py
@@ -4,10 +4,10 @@ from topic import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),        # http://127.0.0.1:8080/admin/
+    path('admin/', admin.site.urls),        # http://127.0.0.1:8080/admin/ - страница Администратора*
 
     # URLS FOR APPLICATION TOPIC
 
-    path("topic/", views.m_topic),          # http://127.0.0.1:8080/topic/
-    path("topic/disc/",views.topic_disc)
+    path("topic/", views.m_topic),          # http://127.0.0.1:8080/topic/ - основная страница
+    path("topic/disc/",views.topic_disc)    # http://127.0.0.1:8080/topic/disc/ - страница описания проекта
 ]
